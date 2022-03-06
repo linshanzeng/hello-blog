8e3c7f78d6a031d857b4e8c92cf318f3c7c2d825
chiyisan <chiyisan@gmail.com>
2022-03-06 20:14:32 +0800

    deploy

---
+++ deployLife.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:linshanzeng/life.git main:main
+
+# 回到docs
+cd -
+
+# 继续上传hello-blog
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:linshanzeng/hello-blog.git main:main
