49ca3028f2e9b1440d158afc04c3b5e11d990d99
chiyisan <chiyisan@gmail.com>
2022-03-08 06:53:48 +0800

    deploy

--- deployBlog.sh
+++ deployBlog.sh
@@ -22,10 +22,9 @@ sed -i -e '/#target03 /c\
 sed -i -e '/target04: /c\
                     clientSecret: "9cc0245b024b4f67740ebe5293bc6141ef2368d5", // #target04
 ' config.js
-cd -                    
 # 标题
 sed -i -e '/#target05/c\
-    title: '博客', // #target05
+    title: "博客", // #target05
 ' config.js
 cd -
 
