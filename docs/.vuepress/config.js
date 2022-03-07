8145c77f59861bae95be8f4c5f0eefc3c8eb4840
chiyisan <chiyisan@gmail.com>
2022-03-08 02:29:22 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: '我的博客',
     description: '记录和回顾常用开发知识',
-    base: '/',
+    base: '/life',
     theme: 'reco',
     locales: {
         '/': {
@@ -19,7 +19,7 @@ module.exports = {
                     { text: 'GitHub', link: 'https://github.com/linshanzeng/hello-blog' },
                     { text: '导航', link: 'https://linshanzeng.github.io/' },
                     { text: '生活', link: 'https://linshanzeng.github.io/life' },
-                    { text: '博客', link: 'https://linshanzeng.github.io/life' },
+                    { text: '博客', link: 'https://linshanzeng.github.io/blog' },
                 ]
             }
         ],
